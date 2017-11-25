6fcef27d87a5c557403227c7e001dc0f199597b5
yugal_jain <yugal.jain@speedboxapp.com>
2017-11-25 18:56:16 +0530

    1st commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+// const emailModule = require('./app/routes/');
+
+const app = express();
+
+// connect to db
+// require('./lib');
+
+// Static path to client files.
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use('/sankit/api/v1/', routes);
+// Our Route server url
+// app.use('/allied/api/v1', router);
+
+// rendering Files
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/pages/`);
+});
+
+module.exports = app;
